2e800a424cd6cd37a69232a892237a66b276d46b
Lucas Chandra <lucas.chandra@outlook.com>
2021-07-23 10:29:36 -0400

    changed equality sign in filtering object id

--- charts.js
+++ charts.js
@@ -60,7 +60,7 @@ function buildCharts(sample) {
     // 3. Create a variable that holds the samples array. 
     var samplesArray = data.sample;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-    var resultArray = samplesArray.filter((obj) => obj.id === sample);
+    var resultArray = samplesArray.filter((obj) => obj.id == sample);
     //  5. Create a variable that holds the first sample in the array.
     var result = resultArray[0];
 
