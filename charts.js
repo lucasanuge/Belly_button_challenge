8221c3e9955056ef78ff0a4260e3a74ccb850692
Lucas Chandra <lucas.chandra@outlook.com>
2021-07-23 15:40:09 -0400

    done

--- charts.js
+++ charts.js
@@ -73,7 +73,7 @@ function buildCharts(sample) {
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = otu_ids.slice(0,11).map(String);
+    var yticks = otu_ids.map(String).slice(0,10);
 
     // 8. Create the trace for the bar chart. 
     var barData = [
