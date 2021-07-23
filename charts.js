da89e93c637595cdbfc67f35568ad23d4fe3c613
Lucas Chandra <lucas.chandra@outlook.com>
2021-07-23 15:35:51 -0400

    finished deliverable 1

--- charts.js
+++ charts.js
@@ -78,8 +78,8 @@ function buildCharts(sample) {
     // 8. Create the trace for the bar chart. 
     var barData = [
       {
-        x : yticks,
-        y : sample_values.slice(0,11),
+        x : sample_values.slice(0,11),
+        y : yticks,
         type : "bar"
       }
     ];
