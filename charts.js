4112b94b8090570f1aba2c3c6015781293e7d977
Lucas Chandra <lucas.chandra@outlook.com>
2021-07-23 10:53:29 -0400

    changed results to result, line 68-70 of charts.js

--- charts.js
+++ charts.js
@@ -65,9 +65,9 @@ function buildCharts(sample) {
     var result = resultArray[0];
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-    var otu_ids =  results.otu_ids;
-    var otu_labels = results.otu_labels;
-    var sample_values = results.sample_values;
+    var otu_ids =  result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
