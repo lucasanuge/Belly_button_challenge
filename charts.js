d070e91ee76741a78fd4b2e8df635a4be95172d8
Lucas Chandra <lucas.chandra@outlook.com>
2021-07-23 10:44:08 -0400

    modified line 63 charts.js

--- charts.js
+++ charts.js
@@ -58,9 +58,9 @@ function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
-    var samplesArray = data.sample;
+    var samplesArray = data.samples;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-    var resultArray = samplesArray.filter((obj) => obj.id == sample);
+    var resultArray = samplesArray.filter(sampleObj => sampleObj.id == sample);
     //  5. Create a variable that holds the first sample in the array.
     var result = resultArray[0];
 
