f5211f382b4975c3042c8bd0afaf1aef29ffad03
Lucas Chandra <lucas.chandra@outlook.com>
2021-07-26 11:56:43 -0400

    second iteration of gauge chart

--- charts.js
+++ charts.js
@@ -122,14 +122,14 @@ function buildCharts(sample) {
     // Gauge
     // 1. Create a variable that filters the metadata array for the object with the desired sample number.
     var metadata = data.metadata;
-    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var resultArrayMeta = metadata.filter(sampleObj => sampleObj.id == sample);
       
 
     // 2. Create a variable that holds the first sample in the metadata array.
-    var result = resultArray[0]
+    var resultMeta = resultArrayMeta[0]
 
     // 3. Create a variable that holds the washing frequency.
-    var wfreq = results.wfreq
+    var wfreq = resultMeta.wfreq
     // 4. Create the trace for the gauge chart.
     var gaugeData = [
      {
